5401d0c48a9c2f6b7c52b2c8c4df1f9871d3c360
Miguel David Tamayo Toro <migueld2000@gmail.com>
2021-11-19 13:52:38 -0500

    se termina los modelos y las consultas

--- src/clubparkingpoint/modelos/Usuario.java
+++ src/clubparkingpoint/modelos/Usuario.java
@@ -9,5 +9,71 @@ package clubparkingpoint.modelos;
  * @author user
  */
 public class Usuario {
-    
+          
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String fechaIngreso;
+    private String fechaSalida;
+
+    public Usuario() {
+    }
+
+    public Usuario(String cedula, String nombre, String apellido, String telefono, String fechaIngreso, String fechaSalida) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.fechaIngreso = fechaIngreso;
+        this.fechaSalida = fechaSalida;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }       
+
+    public String getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(String fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public String getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(String fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
 }
